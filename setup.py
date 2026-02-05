a4bb7fc8e2d2a9db2d9cc53dc08ba5ef9fe7d118
dineshtyagi1511 <dineshtyagi15112002@gmail.com>
2026-02-05 23:27:03 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name = "visa",
+    version="0.0.1",
+    author = "Dinesh Tyagi",
+    author_email="dineahtyagi1511@gmail.com",
+    packages = find_packages()
+)
